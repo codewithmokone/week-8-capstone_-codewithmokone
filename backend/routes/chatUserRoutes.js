027378021c49f55b8141ff4d82794c07913d6c39
Simon Lephoto <codewithmokone@gmail.com>
2025-07-24 13:44:39 +0200

    fixing deploy error caused by chatUserController in routes

--- backend/routes/chatUserRoutes.js
+++ backend/routes/chatUserRoutes.js
@@ -5,7 +5,7 @@ const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', chatUserController.registerUser);
 router.post('/login', chatUserController.loginUser);
-router.delete('/:id', chatUserController.deleteUser);
-router.get('/', chatUserController.getAllUsers);
+// router.delete('/:id', chatUserController.deleteUser);
+// router.get('/', chatUserController.getAllUsers);
 
 module.exports = router;
