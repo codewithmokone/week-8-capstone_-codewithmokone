58eca65546a419dcf8efff105949830cbae6e0ab
Simon Lephoto <codewithmokone@gmail.com>
2025-07-25 13:03:42 +0200

    upadated mongodb reference

--- backend/server.js
+++ backend/server.js
@@ -118,7 +118,7 @@ const server = http.createServer(app);
 // Connect to MongoDB and start server
 mongoose
   // .connect(process.env.MONGODB_URI)
-  .connect(process.env.MONGODB_URI_ATLAS)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
